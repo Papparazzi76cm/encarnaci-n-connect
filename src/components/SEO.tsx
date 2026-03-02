16c592bfaba455e14ea9b196ed904ecccc54bb14
Carlos Mariscal <mariscalimagen@gmail.com>
2026-03-02 19:46:56 +0100

    Create SEO.tsx

---
+++ src/components/SEO.tsx
@@ -0,0 +1,41 @@
+import { Helmet } from "react-helmet-async";
+
+interface SEOProps {
+  title: string;
+  description: string;
+  keywords?: string;
+  url?: string;
+  schema?: object;
+}
+
+const SEO = ({ title, description, keywords, url = "https://encarnacioninmobiliaria.com", schema }: SEOProps) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      {keywords && <meta name="keywords" content={keywords} />}
+      <link rel="canonical" href={url} />
+      
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:url" content={url} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+
+      {/* Structured Data (JSON-LD) */}
+      {schema && (
+        <script type="application/ld+json">
+          {JSON.stringify(schema)}
+        </script>
+      )}
+    </Helmet>
+  );
+};
+
+export default SEO;
